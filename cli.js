82248a567e5edc4e35c78d9faa484c60293c421f
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-16 00:09:47 +0900

    first implement

--- cli.js
+++ cli.js
@@ -1,30 +1,51 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
-var pkg = require('../package.json')
-var psg = require('..')
+var pkg = require('./package.json')
+var psg = require('./')
 
 var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2), {
-    boolean: [
-        'help',
-        'versions'
-    ],
-    alias: {
-        h: 'help',
-        V: 'versions'
-    }
+  boolean: [
+    'help',
+    'versions'
+  ],
+  alias: {
+    h: 'help',
+    V: 'versions'
+  }
 })
 
+
 if (argv.V) {
-    console.log(pkg.version)
+  console.log(pkg.version)
 }
 
+
 if (argv.h) {
-    console.log('Usage: atcss input-name output-name [options]');
-    console.log('');
-    console.log('Options:');
-    console.log('');
-    console.log('  -V, --versions    output the version number');
-    console.log('  -h, --help        output usage information');
+  console.log('Usage: atcss input-name output-name [options]')
+  console.log('')
+  console.log('Options:')
+  console.log('')
+  console.log('  -V, --versions    output the version number')
+  console.log('  -h, --help        output usage information')
+}
+
+
+var opts = {}
+
+if (argv.theme) {
+  opts.theme = argv.theme
+}
+if (argv.name) {
+  opts.name = argv.name
+}
+if (argv.file) {
+  opts.file = argv.file
+}
+
+
+if (argv._[0]) {
+  var css = fs.readFileSync(argv._[0], 'utf-8')
+  psg(css, opts)
 }
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-module.exports = function () {
+var fs = require('fs')
+var postcss = require('postcss')
+var psg = require('postcss-style-guide')
+
+module.exports = function (css, opts) {
+  postcss().use(psg(css, opts)).process(css).css
 }
