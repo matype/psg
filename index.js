018173f82ff32555d4c30ee8b8188b99a59d613d
morishitter <morishitter@gmail.com>
2015-07-21 23:36:58 +0900

    simplify

--- index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
-var postcss = require('postcss')
-var psg = require('postcss-style-guide')
-
-module.exports = function (css, opts) {
-  postcss().use(psg(css, opts)).process(css).css
-}
